bf395e22748a2fb2822b5df6a66f7cc69230541d
Shikhar Mutta <shikharmutta67@gmail.com>
2026-05-29 22:19:51 +0530

    U10

--- 860_driver.cpp
+++ 860_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "832.cpp"
+#include "860.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -41,7 +41,7 @@ vector<vector<int>> _rvvi() {
     vector<vector<int>> v;
     int dep=0; string cur;
     for (char c : s) {
-        if (c=='[') { dep++; if(dep>2) cur+=c; }
+        if (c=='[') { dep++; if(dep>1) cur+=c; }
         else if (c==']') {
             dep--;
             if (dep==1) {
@@ -59,10 +59,10 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        auto image = _rvvi();
+        auto bills = _rvi();
         Solution sol;
-        auto res = sol.flipAndInvertImage(image);
-        for(auto&row:res){for(int _i=0;_i<(int)row.size();_i++){if(_i)cout<<" ";cout<<row[_i];}cout<<"\n";}
+        auto res = sol.lemonadeChange(bills);
+        cout << (res ? "true" : "false") << "\n";
     }
     return 0;
 }

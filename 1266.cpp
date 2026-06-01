16aaddc0ada041c793269342f4555d31d509cf25
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-01 22:56:18 +0530

    U13

---
+++ 1266.cpp
@@ -0,0 +1,11 @@
+// Link: https://leetcode.com/problems/minimum-time-visiting-all-points/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        
+    }
+};

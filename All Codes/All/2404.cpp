3cdc98d1d324f85a0e8678ff94162491d6306cf5
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-04 12:50:52 +0530

    U4

---
+++ All Codes/All/2404.cpp
@@ -0,0 +1,39 @@
+// Link: https://leetcode.com/problems/most-frequent-even-element/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // Approach: use array as frequency map. TC: O(n), SC: O(1)
+    int mostFrequentEven(vector<int> &nums)
+    {
+        int freq_map[100001] = {0};
+        int count = 0;
+        int max = -1;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+
+                freq_map[nums[i]]++;
+
+                if (freq_map[nums[i]] > count)
+                {
+
+                    count = freq_map[nums[i]];
+                    max = nums[i];
+                }
+
+                else if (freq_map[nums[i]] == count)
+                {
+                    max = min(nums[i], max);
+                }
+            }
+        }
+
+        return max;
+    }
+};

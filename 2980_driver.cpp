a1825cb75ea458c47678d425a523fc39a01a8d1d
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-05 22:57:03 +0530

    U11

--- 2980_driver.cpp
+++ 2980_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "2974.cpp"
+#include "2980.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -61,8 +61,8 @@ int main() {
     while (t--) {
         auto nums = _rvi();
         Solution sol;
-        auto res = sol.numberGame(nums);
-        for (int _i=0;_i<(int)res.size();_i++){if(_i)cout<<" ";cout<<res[_i];}cout<<"\n";
+        auto res = sol.hasTrailingZeros(nums);
+        cout << (res ? "true" : "false") << "\n";
     }
     return 0;
 }

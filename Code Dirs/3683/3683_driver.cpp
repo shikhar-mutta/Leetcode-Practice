fc2206da088f94775288b68f0ff83a32246bb8ba
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-08 19:00:31 +0530

    U4

--- Code Dirs/3683/3683_driver.cpp
+++ Code Dirs/3683/3683_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "3678.cpp"
+#include "3683.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,9 +59,9 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        auto nums = _rvi();
+        auto tasks = _rvvi();
         Solution sol;
-        auto res = sol.smallestAbsent(nums);
+        auto res = sol.earliestTime(tasks);
         cout << res << "\n";
     }
     return 0;

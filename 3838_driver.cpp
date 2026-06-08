30a5f90c9574d092bd5bcd4b812a4d019687439e
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-08 20:19:04 +0530

    U10

--- 3838_driver.cpp
+++ 3838_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "3769.cpp"
+#include "3838.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,10 +59,11 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        auto nums = _rvi();
+        auto words = _rvs();
+        auto weights = _rvi();
         Solution sol;
-        auto res = sol.sortByReflection(nums);
-        for (int _i=0;_i<(int)res.size();_i++){if(_i)cout<<" ";cout<<res[_i];}cout<<"\n";
+        auto res = sol.mapWordWeights(words, weights);
+        cout << res << "\n";
     }
     return 0;
 }

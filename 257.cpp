ef81303d49ff0ec1e25897c8c7d9345d8547af4b
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-09 20:16:04 +0530

    U7

--- 257.cpp
+++ 257.cpp
@@ -19,21 +19,12 @@ public:
     vector<string> binaryTreePaths(TreeNode *root)
     {
         vector<string> ans;
-        if (!root)
-            return ans;
-        queue<pair<TreeNode *, string>> q;
-        q.push({root, to_string(root->val)});
-        while (!q.empty())
-        {
-            auto [node, path] = q.front();
-            q.pop();
-            if (!node->left && !node->right)
-                ans.push_back(path);
-            if (node->left)
-                q.push({node->left, path + "->" + to_string(node->left->val)});
-            if (node->right)
-                q.push({node->right, path + "->" + to_string(node->right->val)});
-        }
+        function<void(TreeNode*, string)> dfs = [&](TreeNode* node, string path) {
+            if (!node->left && !node->right) { ans.push_back(path); return; }
+            if (node->left)  dfs(node->left,  path + "->" + to_string(node->left->val));
+            if (node->right) dfs(node->right, path + "->" + to_string(node->right->val));
+        };
+        if (root) dfs(root, to_string(root->val));
         return ans;
     }
 };

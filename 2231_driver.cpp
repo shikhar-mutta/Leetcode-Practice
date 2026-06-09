1f22f7e4b4d30e82fa2416faf8fe24f381a99e27
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-09 19:09:26 +0530

    U2

--- 2231_driver.cpp
+++ 2231_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "1859.cpp"
+#include "2231.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,9 +59,9 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        string s = _rs();
+        int num = _ri();
         Solution sol;
-        auto res = sol.sortSentence(s);
+        auto res = sol.largestInteger(num);
         cout << res << "\n";
     }
     return 0;

a84f2a8e715f3c5c7ac14c5eb0efaf97ea0bbd35
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-16 22:05:21 +0530

    U8

--- Code Dirs/3582/3582_driver.cpp
+++ Code Dirs/3582/3582_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "3541.cpp"
+#include "3582.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,9 +59,9 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        string s = _rs();
+        string caption = _rs();
         Solution sol;
-        auto res = sol.maxFreqSum(s);
+        auto res = sol.generateTag(caption);
         cout << res << "\n";
     }
     return 0;

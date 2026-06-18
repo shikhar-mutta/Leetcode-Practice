4d1d6e927a6710e24c6abd5fe751cced1df33d9a
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-18 22:38:38 +0530

    U11

---
+++ All Codes/All/852.cpp
@@ -0,0 +1,22 @@
+// Link: https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int low = 0, high = arr.size() - 1;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] < arr[mid + 1])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low;
+    }
+};

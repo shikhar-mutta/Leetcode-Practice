ea6c305890e15460881d1d955e7efd83e1a4c578
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-20 21:18:40 +0530

    U10

--- 401_driver.cpp
+++ 401_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "342.cpp"
+#include "401.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,10 +59,10 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        int n = _ri();
+        int turnedOn = _ri();
         Solution sol;
-        auto res = sol.isPowerOfFour(n);
-        cout << (res ? "true" : "false") << "\n";
+        auto res = sol.readBinaryWatch(turnedOn);
+        for (int _i=0;_i<(int)res.size();_i++){if(_i)cout<<" ";cout<<res[_i];}cout<<"\n";
     }
     return 0;
 }

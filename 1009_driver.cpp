f8ed126f4df52f238d5c1d98ee47200044422ffa
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-21 21:43:11 +0530

    U10

--- 1009_driver.cpp
+++ 1009_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "1342.cpp"
+#include "1009.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,9 +59,9 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        int num = _ri();
+        int n = _ri();
         Solution sol;
-        auto res = sol.numberOfSteps(num);
+        auto res = sol.bitwiseComplement(n);
         cout << res << "\n";
     }
     return 0;

d488d5d33b0f5c96217eab48da61c950f3399b3f
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-23 01:25:45 +0530

    23rd June

--- 3858_driver.cpp
+++ 3858_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "3882.cpp"
+#include "3858.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -61,7 +61,7 @@ int main() {
     while (t--) {
         auto grid = _rvvi();
         Solution sol;
-        auto res = sol.minCost(grid);
+        auto res = sol.minimumOR(grid);
         cout << res << "\n";
     }
     return 0;

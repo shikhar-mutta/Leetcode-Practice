4cb8f4b49a5ed451052c79a6fd2b5788cf7736ba
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-23 14:09:56 +0530

    U4

---
+++ 3702.cpp
@@ -0,0 +1,12 @@
+// Link: https://leetcode.com/problems/longest-subsequence-with-non-zero-bitwise-xor/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestSubsequence(vector<int> &nums)
+    {
+    }
+};

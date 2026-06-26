13014b875e4fd0df66e07f2bb80b6b98814d3b18
Shikhar Mutta <shikharmutta67@gmail.com>
2026-06-26 22:41:58 +0530

    U10

--- 678_driver.cpp
+++ 678_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "347.cpp"
+#include "678.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,11 +59,10 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        auto nums = _rvi();
-        int k = _ri();
+        string s = _rs();
         Solution sol;
-        auto res = sol.topKFrequent(nums, k);
-        for (int _i=0;_i<(int)res.size();_i++){if(_i)cout<<" ";cout<<res[_i];}cout<<"\n";
+        auto res = sol.checkValidString(s);
+        cout << (res ? "true" : "false") << "\n";
     }
     return 0;
 }

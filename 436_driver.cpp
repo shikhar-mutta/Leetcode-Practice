0b21a7d7287094791e9dd60eac715f35b870f3e0
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-07 23:57:08 +0530

    U9

--- 436_driver.cpp
+++ 436_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "220.cpp"
+#include "436.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,12 +59,10 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        auto nums = _rvi();
-        int indexDiff = _ri();
-        int valueDiff = _ri();
+        auto intervals = _rvvi();
         Solution sol;
-        auto res = sol.containsNearbyAlmostDuplicate(nums, indexDiff, valueDiff);
-        cout << (res ? "true" : "false") << "\n";
+        auto res = sol.findRightInterval(intervals);
+        for (int _i=0;_i<(int)res.size();_i++){if(_i)cout<<" ";cout<<res[_i];}cout<<"\n";
     }
     return 0;
 }

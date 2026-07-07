af9f60ed43a4356c52aa3d8525265d5c340d288c
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-08 02:20:23 +0530

    U7

--- 532_driver.cpp
+++ 532_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "524.cpp"
+#include "532.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,10 +59,10 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        string s = _rs();
-        auto dictionary = _rvs();
+        auto nums = _rvi();
+        int k = _ri();
         Solution sol;
-        auto res = sol.findLongestWord(s, dictionary);
+        auto res = sol.findPairs(nums, k);
         cout << res << "\n";
     }
     return 0;

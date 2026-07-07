91483e0fade41ad8d1bba8ffa991e548816189ea
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-07 23:49:54 +0530

    U6

--- 215_driver.cpp
+++ 215_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "179.cpp"
+#include "215.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -60,8 +60,9 @@ int main() {
     cin.ignore();
     while (t--) {
         auto nums = _rvi();
+        int k = _ri();
         Solution sol;
-        auto res = sol.largestNumber(nums);
+        auto res = sol.findKthLargest(nums, k);
         cout << res << "\n";
     }
     return 0;

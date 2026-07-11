3d48e9c17d51384eceb9e7efc5529b908966eee0
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-11 19:36:31 +0530

    11th July

--- 945_driver.cpp
+++ 945_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "939.cpp"
+#include "945.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,9 +59,9 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        auto points = _rvvi();
+        auto nums = _rvi();
         Solution sol;
-        auto res = sol.minAreaRect(points);
+        auto res = sol.minIncrementForUnique(nums);
         cout << res << "\n";
     }
     return 0;

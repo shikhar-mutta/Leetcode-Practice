c68ee5a6ed9d7a38d1da1b1a2d7aa443b0c4011b
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-16 22:10:17 +0530

    U5

--- 1878_driver.cpp
+++ 1878_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "1877.cpp"
+#include "1878.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,10 +59,10 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        auto nums = _rvi();
+        auto grid = _rvvi();
         Solution sol;
-        auto res = sol.minPairSum(nums);
-        cout << res << "\n";
+        auto res = sol.getBiggestThree(grid);
+        for (int _i=0;_i<(int)res.size();_i++){if(_i)cout<<" ";cout<<res[_i];}cout<<"\n";
     }
     return 0;
 }

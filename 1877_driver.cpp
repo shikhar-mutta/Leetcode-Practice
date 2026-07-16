9a0ef7177e50718934b381a967e711f115fdcfe1
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-16 22:04:42 +0530

    U4

--- 1877_driver.cpp
+++ 1877_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "1846.cpp"
+#include "1877.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,9 +59,9 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        auto arr = _rvi();
+        auto nums = _rvi();
         Solution sol;
-        auto res = sol.maximumElementAfterDecrementingAndRearranging(arr);
+        auto res = sol.minPairSum(nums);
         cout << res << "\n";
     }
     return 0;

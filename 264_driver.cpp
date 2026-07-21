ffb93612f3381831a62e2461fcb35538a4951bf2
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-21 07:16:32 +0530

    U8

--- 264_driver.cpp
+++ 264_driver.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "273.cpp"
+#include "264.cpp"
 
 // ── read helpers ──────────────────────────────────────────────────
 int           _ri()  { string s; getline(cin,s); return stoi(s); }
@@ -59,9 +59,9 @@ int main() {
     cin >> t;
     cin.ignore();
     while (t--) {
-        int num = _ri();
+        int n = _ri();
         Solution sol;
-        auto res = sol.numberToWords(num);
+        auto res = sol.nthUglyNumber(n);
         cout << res << "\n";
     }
     return 0;

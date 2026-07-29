93e8b16575845fd19432175155399087f1908c93
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-29 21:37:46 +0530

    U10

--- All Codes/Days/d64/1964.cpp
+++ All Codes/Days/d64/1964.cpp
@@ -3,24 +3,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Added
 // TC: O(n log n)  SC: O(n)
-// Approach: patience-sorting LIS variant for non-decreasing subsequences.
-// Maintain tails[] of the smallest tail value for each subsequence length;
-// for each obstacle, upper_bound finds where it replaces (allows equal
-// values to extend), giving that position's answer as index+1.
-class Solution {
+//  Approach: patience-sorting LIS variant for non-decreasing subsequences.
+//  Maintain tails[] of the smallest tail value for each subsequence length;
+//  for each obstacle, upper_bound finds where it replaces (allows equal
+//  values to extend), giving that position's answer as index+1.
+class Solution
+{
 public:
-    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
-        vector<int> tails;
-        vector<int> res;
-        for (int x : obstacles) {
-            auto it = upper_bound(tails.begin(), tails.end(), x);
-            int idx = it - tails.begin();
-            if (it == tails.end()) tails.push_back(x);
-            else *it = x;
-            res.push_back(idx + 1);
+    vector<int> longestObstacleCourseAtEachPosition(vector<int> &obstacles)
+    {
+        vector<int> lis(obstacles.size(), 1);
+        vector<int> temp;
+        for (int i = 0; i < obstacles.size(); i++)
+        {
+            if (temp.size() == 0 || obstacles[i] >= temp.back())
+            {
+                temp.push_back(obstacles[i]);
+                lis[i] = temp.size();
+            }
+            else
+            {
+                auto it = upper_bound(temp.begin(), temp.end(), obstacles[i]);
+                int ind = it - temp.begin();
+                temp[ind] = obstacles[i];
+                lis[i] = ind + 1;
+            }
         }
-        return res;
+        return lis;
     }
 };

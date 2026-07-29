cf8604e5ce497e466c67ab54d8ada68fcda2fe21
Shikhar Mutta <shikharmutta67@gmail.com>
2026-07-29 19:08:12 +0530

    U1

--- All Codes/Days/d64/1728.cpp
+++ All Codes/Days/d64/1728.cpp
@@ -3,148 +3,145 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+// TC: O(m*n*turns) SC: O(m*n*turns)
+//  Approach: BFS + DP
+//   We can use BFS to simulate the game. We can use DP to store the state of the game. We can use a queue to store the state of the game. We can use a 3D array to store the state of the game. The first dimension is the turn, the second dimension is the position of the mouse, and the third dimension is the position of the cat. We can use a 2D array to store the number of incoming edges for each state.
+class Solution
+{
 public:
-    bool canMouseWin(vector<string>& grid, int catJump, int mouseJump) {
-        int R = grid.size(), C = grid[0].size();
-        int foodR = -1, foodC = -1;
-        int mouseR = -1, mouseC = -1, catR = -1, catC = -1;
-        for (int i = 0; i < R; i++)
-            for (int j = 0; j < C; j++) {
-                if (grid[i][j] == 'F') { foodR = i; foodC = j; }
-                else if (grid[i][j] == 'M') { mouseR = i; mouseC = j; }
-                else if (grid[i][j] == 'C') { catR = i; catC = j; }
+    int m, n, food, cat, mouse;
+    bool outBounds(int x, int y) { return x < 0 || y < 0 || x >= m || y >= n; }
+
+    bool canMouseWin(vector<string> &grid, int cj, int mj)
+    {
+        m = grid.size(), n = grid[0].size(), food = 0, cat = 0, mouse = 0;
+        int dp[2][m * n][m * n], insc[m * n][m * n], ins[m * n], bad[m * n];
+        memset(dp, 0, sizeof(dp));
+        fill(&insc[0][0], &insc[0][0] + (m * n) * (m * n), 1);
+        memset(bad, 0, sizeof(bad));
+        memset(ins, 0, sizeof(ins));
+
+        vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (grid[i][j] == 'F')
+                    food = i * n + j;
+                if (grid[i][j] == 'C')
+                    cat = i * n + j;
+                if (grid[i][j] == 'M')
+                    mouse = i * n + j;
+                if (grid[i][j] == 'F' || grid[i][j] == '#')
+                    bad[i * n + j] = 1;
             }
+        }
 
-        auto enc = [&](int mr, int mc, int cr, int cc, int turn) {
-            return (((mr * C + mc) * R * C + (cr * C + cc)) * 2 + turn);
-        };
-
-        int N = R * C * R * C * 2;
-        vector<int> color(N, 0); // 0 unknown, 1 mouse win, 2 cat win
-        vector<int> degree(N, 0);
-
-        int dx[5] = {0, 0, 0, 1, -1};
-        int dy[5] = {0, 1, -1, 0, 0};
-
-        auto validMoves = [&](int r, int c, int jump, int turn) {
-            vector<pair<int,int>> res;
-            res.push_back({r, c}); // stay
-            for (int d = 1; d <= 4; d++) {
-                for (int step = 1; step <= jump; step++) {
-                    int nr = r + dx[d] * step, nc = c + dy[d] * step;
-                    if (nr < 0 || nr >= R || nc < 0 || nc >= C || grid[nr][nc] == '#') break;
-                    res.push_back({nr, nc});
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                int curr = i * n + j;
+                if (bad[curr])
+                    continue;
+                for (auto [a, b] : dir)
+                {
+                    for (int len = 1; len <= cj; ++len)
+                    {
+                        int x = len * a + i, y = len * b + j, z = x * n + y;
+                        if (outBounds(x, y) || (grid[x][y] != 'F' && bad[z]))
+                            break;
+                        ++ins[curr];
+                    }
                 }
-            }
-            return res;
-        };
 
-        // compute degree for all states
-        for (int mr = 0; mr < R; mr++) {
-            if (grid[mr][0] == '#' && C > 0) {} // no-op just to keep structure
-            for (int mc = 0; mc < C; mc++) {
-                if (grid[mr][mc] == '#') continue;
-                for (int cr = 0; cr < R; cr++) {
-                    for (int cc = 0; cc < C; cc++) {
-                        if (grid[cr][cc] == '#') continue;
-                        for (int turn = 0; turn < 2; turn++) {
-                            int st = enc(mr, mc, cr, cc, turn);
-                            if (turn == 0) degree[st] = validMoves(mr, mc, mouseJump, 0).size();
-                            else degree[st] = validMoves(cr, cc, catJump, 1).size();
-                        }
+                for (int ii = 0; ii < m; ++ii)
+                {
+                    for (int jj = 0; jj < n; ++jj)
+                    {
+                        int rruc = ii * n + jj;
+                        if (bad[rruc] || rruc == curr)
+                            continue;
+                        insc[rruc][curr] += ins[curr];
                     }
                 }
             }
         }
 
-        queue<int> q;
-        for (int mr = 0; mr < R; mr++) {
-            for (int mc = 0; mc < C; mc++) {
-                if (grid[mr][mc] == '#') continue;
-                for (int cr = 0; cr < R; cr++) {
-                    for (int cc = 0; cc < C; cc++) {
-                        if (grid[cr][cc] == '#') continue;
-                        for (int turn = 0; turn < 2; turn++) {
-                            int st = enc(mr, mc, cr, cc, turn);
-                            if (mr == cr && mc == cc) {
-                                color[st] = 2; // cat wins
-                                q.push(st);
-                            } else if (mr == foodR && mc == foodC) {
-                                color[st] = 1; // mouse wins
-                                q.push(st);
+        queue<tuple<int, int, int>> q;
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                int curr = i * n + j;
+                if (bad[curr])
+                    continue;
+                dp[0][food][curr] = 1;
+                q.emplace(0, food, curr);
+            }
+        }
+
+        int turns = 0;
+        while (!q.empty() && turns < 1000)
+        {
+            int sz = q.size();
+            while (sz--)
+            {
+                auto [t, x, y] = q.front();
+                q.pop();
+                if (t)
+                {
+                    int i = y / n, j = y % n;
+                    --insc[x][y];
+                    if (insc[x][y] == 0 && !dp[0][x][y])
+                    {
+                        dp[0][x][y] = 1;
+                        q.emplace(0, x, y);
+                    }
+
+                    for (auto [a, b] : dir)
+                    {
+                        for (int len = 1; len <= cj; ++len)
+                        {
+                            int nx = len * a + i, ny = len * b + j,
+                                nz = nx * n + ny;
+                            if (outBounds(nx, ny) || bad[nz])
+                                break;
+                            if (dp[0][x][nz] || x == nz)
+                                continue;
+                            --insc[x][nz];
+                            if (insc[x][nz] == 0)
+                            {
+                                dp[0][x][nz] = 1;
+                                q.emplace(0, x, nz);
                             }
+                        }
+                    }
+                }
+                else
+                {
+                    int i = x / n, j = x % n;
+                    for (auto [a, b] : dir)
+                    {
+                        for (int len = 1; len <= mj; ++len)
+                        {
+                            int nx = len * a + i, ny = len * b + j,
+                                nz = nx * n + ny;
+                            if (outBounds(nx, ny) || bad[nz])
+                                break;
+                            if (dp[1][nz][y] || nz == y)
+                                continue;
+                            dp[1][nz][y] = 1;
+                            q.emplace(1, nz, y);
                         }
                     }
                 }
             }
+
+            ++turns;
         }
 
-        // predecessor generation: for a state (mr,mc,cr,cc,turn) that just got resolved,
-        // its predecessors are states with the opposite turn where the mover could have moved into this position.
-        while (!q.empty()) {
-            int st = q.front(); q.pop();
-            int result = color[st];
-            int turn = st % 2;
-            int rest = st / 2;
-            int catPos = rest % (R * C);
-            int mousePos = rest / (R * C);
-            int mr = mousePos / C, mc = mousePos % C;
-            int cr = catPos / C, cc = catPos % C;
-
-            int prevTurn = 1 - turn;
-            // predecessor: prevTurn's mover was at some position, moved to (mr,mc) if prevTurn==0(mouse) matching current mr,mc
-            // and cat/mouse pos not-moved stays same
-            if (prevTurn == 0) {
-                // mouse moved to (mr,mc) from some (pr,pc); cat position cr,cc unchanged
-                for (int pr = 0; pr < R; pr++) {
-                    for (int pc = 0; pc < C; pc++) {
-                        if (grid[pr][pc] == '#') continue;
-                        auto moves = validMoves(pr, pc, mouseJump, 0);
-                        bool can = false;
-                        for (auto& mv : moves) if (mv.first == mr && mv.second == mc) { can = true; break; }
-                        if (!can) continue;
-                        int pst = enc(pr, pc, cr, cc, prevTurn);
-                        if (color[pst] != 0) continue;
-                        if (result == 1) {
-                            color[pst] = 1;
-                            q.push(pst);
-                        } else {
-                            degree[pst]--;
-                            if (degree[pst] == 0) {
-                                color[pst] = 2;
-                                q.push(pst);
-                            }
-                        }
-                    }
-                }
-            } else {
-                // cat moved to (cr,cc) from some (pr,pc); mouse position unchanged
-                for (int pr = 0; pr < R; pr++) {
-                    for (int pc = 0; pc < C; pc++) {
-                        if (grid[pr][pc] == '#') continue;
-                        auto moves = validMoves(pr, pc, catJump, 1);
-                        bool can = false;
-                        for (auto& mv : moves) if (mv.first == cr && mv.second == cc) { can = true; break; }
-                        if (!can) continue;
-                        int pst = enc(mr, mc, pr, pc, prevTurn);
-                        if (color[pst] != 0) continue;
-                        if (result == 2) {
-                            color[pst] = 2;
-                            q.push(pst);
-                        } else {
-                            degree[pst]--;
-                            if (degree[pst] == 0) {
-                                color[pst] = 1;
-                                q.push(pst);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        int startState = enc(mouseR, mouseC, catR, catC, 0);
-        return color[startState] == 1;
+        return dp[1][mouse][cat];
     }
 };
--- All Codes/Days/d64/1735.cpp
+++ All Codes/Days/d64/1735.cpp
@@ -3,41 +3,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+// TC: O(n*sqrt(k)) SC: O(n)
+//  Approach: Combinatorics + Modular Arithmetic
+//   We can use combinatorics to solve this problem. We can use the formula for combinations to calculate the number of ways to fill the array. We can use modular arithmetic to avoid overflow. We can use Fermat's little theorem to calculate the modular inverse. We can precompute the factorials and their inverses to speed up the calculations.
+class Solution
+{
     const long long MOD = 1e9 + 7;
     vector<long long> fact, inv_fact;
 
-    long long power(long long b, long long e) {
-        long long r = 1; b %= MOD;
-        while (e) { if (e & 1) r = r * b % MOD; b = b * b % MOD; e >>= 1; }
+    long long power(long long b, long long e)
+    {
+        long long r = 1;
+        b %= MOD;
+        while (e)
+        {
+            if (e & 1)
+                r = r * b % MOD;
+            b = b * b % MOD;
+            e >>= 1;
+        }
         return r;
     }
 
-    long long C(int n, int r) {
-        if (r < 0 || r > n) return 0;
-        return fact[n] * inv_fact[r] % MOD * inv_fact[n-r] % MOD;
+    long long C(int n, int r)
+    {
+        if (r < 0 || r > n)
+            return 0;
+        return fact[n] * inv_fact[r] % MOD * inv_fact[n - r] % MOD;
     }
+
 public:
-    vector<int> waysToFillArray(vector<vector<int>>& queries) {
+    vector<int> waysToFillArray(vector<vector<int>> &queries)
+    {
         int maxN = 10014;
         fact.assign(maxN, 1);
-        for (int i = 1; i < maxN; i++) fact[i] = fact[i-1] * i % MOD;
+        for (int i = 1; i < maxN; i++)
+            fact[i] = fact[i - 1] * i % MOD;
         inv_fact.assign(maxN, 1);
-        inv_fact[maxN-1] = power(fact[maxN-1], MOD - 2);
-        for (int i = maxN - 2; i >= 0; i--) inv_fact[i] = inv_fact[i+1] * (i+1) % MOD;
+        inv_fact[maxN - 1] = power(fact[maxN - 1], MOD - 2);
+        for (int i = maxN - 2; i >= 0; i--)
+            inv_fact[i] = inv_fact[i + 1] * (i + 1) % MOD;
 
         vector<int> res;
-        for (auto& q : queries) {
+        for (auto &q : queries)
+        {
             int n = q[0], k = q[1];
             long long ways = 1;
-            for (int p = 2; (long long)p * p <= k; p++) {
-                if (k % p == 0) {
+            for (int p = 2; (long long)p * p <= k; p++)
+            {
+                if (k % p == 0)
+                {
                     int e = 0;
-                    while (k % p == 0) { k /= p; e++; }
+                    while (k % p == 0)
+                    {
+                        k /= p;
+                        e++;
+                    }
                     ways = ways * C(e + n - 1, n - 1) % MOD;
                 }
             }
-            if (k > 1) ways = ways * C(1 + n - 1, n - 1) % MOD;
+            if (k > 1)
+                ways = ways * C(1 + n - 1, n - 1) % MOD;
             res.push_back((int)ways);
         }
         return res;
